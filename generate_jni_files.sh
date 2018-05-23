54ed8df3e6009e4511d2349c1c5a7fe71db29ba9
Marcin Miotk <mami@snmill.com>
2018-05-23 11:27:50 +0200

    Header files

--- generate_jni_files.sh
+++ generate_jni_files.sh
@@ -1,6 +1,6 @@
 cd target/classes/
-javah -d src/main/resources/includes/ com.snmill.evs.EvsDecoderJni
-javah -d src/main/resources/includes/ com.snmill.evs.EvsEncoderJni
+javah -d ../../src/main/resources/includes/ com.snmill.evs.EvsDecoderJni
+javah -d ../../src/main/resources/includes/ com.snmill.evs.EvsEncoderJni
 cd ..
 cd ..
 
