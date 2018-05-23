0c546d79316d1f5ba3e659463aa2544bdc894509
Marcin Miotk <mami@snmill.com>
2018-05-23 11:42:59 +0200

    Fake implementation of JNI methods

---
+++ evsinc/evsinc/com_snmill_evs_EvsDecoderJni.c
@@ -0,0 +1,33 @@
+#include "com_snmill_evs_EvsDecoderJni.h"
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+/*
+ * Class:     com_snmill_evs_EvsDecoderJni
+ * Method:    dCreate
+ * Signature: ()J
+ */
+JNIEXPORT jlong JNICALL Java_com_snmill_evs_EvsDecoderJni_dCreate
+  (JNIEnv *env, jclass thisObject) {
+    
+    jlong handlerAddress = 77;
+    return handlerAddress;
+}
+
+/*
+ * Class:     com_snmill_evs_EvsDecoderJni
+ * Method:    dDestroy
+ * Signature: (J)V
+ */
+JNIEXPORT void JNICALL Java_com_snmill_evs_EvsDecoderJni_dDestroy
+  (JNIEnv *env, jclass thisObject, jlong handlerAddress) {
+    
+    
+    
+}
+
+#ifdef __cplusplus
+}
+#endif
---
+++ evsinc/evsinc/com_snmill_evs_EvsEncoderJni.c
@@ -0,0 +1,29 @@
+#include "com_snmill_evs_EvsEncoderJni.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+/*
+ * Class:     com_snmill_evs_EvsEncoderJni
+ * Method:    eCreate
+ * Signature: ()J
+ */
+JNIEXPORT jlong JNICALL Java_com_snmill_evs_EvsEncoderJni_eCreate
+  (JNIEnv *env, jclass thisObject) {
+    jlong handlerAddress = 99;
+    return handlerAddress;
+}
+
+/*
+ * Class:     com_snmill_evs_EvsEncoderJni
+ * Method:    eDestroy
+ * Signature: (J)V
+ */
+JNIEXPORT void JNICALL Java_com_snmill_evs_EvsEncoderJni_eDestroy
+  (JNIEnv *env, jclass thisObject, jlong handlerAddress) {
+    
+}
+
+#ifdef __cplusplus
+}
+#endif
